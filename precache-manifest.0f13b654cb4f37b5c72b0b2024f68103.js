a3dbd38f84b7a31989a95b78989e90813bec959b
marvynthewall <jim51507jim@gmail.com>
2019-01-30 00:10:14 +0800

    Updates

---
+++ precache-manifest.0f13b654cb4f37b5c72b0b2024f68103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d9b93e5289f6999c537",
+    "url": "/static/js/main.5d9b93e5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5d9b93e5289f6999c537",
+    "url": "/static/css/main.d5b63631.chunk.css"
+  },
+  {
+    "revision": "022758bc072774d77fa8227bac7f2a85",
+    "url": "/index.html"
+  }
+];
